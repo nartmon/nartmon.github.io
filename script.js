6933bd1ff1aabf29d8b189715c6a894499cf6c13
nartmon <148832994+nartmon@users.noreply.github.com>
2023-11-02 16:00:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 import data from './data.js';
 
-console.log(data[0].number)
 
 const gun = document.getElementsByClassName("gun");
 const boom = document.getElementsByClassName("boom");
-const boomImageList = ["./_src/boom1.png", "./_src/boom2.png", "./_src/boom3.png", "./_src/boom4.png", "./_src/boom5.png"]
+const boomImageList = ["./src/boom1.png", "./src/boom2.png", "./src/boom3.png", "./src/boom4.png", "./src/boom5.png", "./src/boom6.png", "./src/boom7.png", "./src/boom8.png", "./src/boom9.png", "./src/boom10.png"]
 
 const content = document.querySelector('.content');
+
+const slide = document.querySelector('.slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const slideWidth = document.querySelector('.future').clientWidth;
+
 
 content.addEventListener("click", function(event) {
     const x = event.pageX
@@ -14,7 +19,7 @@ content.addEventListener("click", function(event) {
 
     /*gun*/
     const gunImage = document.createElement('img');
-    gunImage.src = "./_src/bullet.png";
+    gunImage.src = "./src/bullet.png";
     gunImage.style.width = "12px"
     gunImage.style.height = "12px"
     gunImage.style.left = (x - 10) + "px";
@@ -23,13 +28,13 @@ content.addEventListener("click", function(event) {
 
     /*boom*/
 
-    let boomImageSrc = boomImageList[Math.floor(Math.random() * 5)]
+    let boomImageSrc = boomImageList[Math.floor(Math.random() * boomImageList.length)]
     const boomImage = document.createElement('img');
     boomImage.src = boomImageSrc
-    boomImage.style.width = "200px"
-    boomImage.style.height = "200px"
+    boomImage.style.width = 240 + "px"
+    boomImage.style.height = "auto"
     boomImage.style.left = (x - 100)+ "px";
-    boomImage.style.top = (y - 100)  + "px";
+    boomImage.style.top = (y - 110)  + "px";
     boom[0].appendChild(boomImage);
 });
 
@@ -37,7 +42,7 @@ content.addEventListener("click", function(event) {
 const gallery = document.querySelector('.gallery');
 const items = gallery.querySelectorAll('li');
 items.forEach(item => {
-    const randomAngle = Math.floor(Math.random() * 2.5 - 1.25);
+    const randomAngle = Math.floor(Math.random() * 0);
     item.style.transform = `rotate(${randomAngle}deg)`;
 });
 
@@ -48,13 +53,58 @@ const docs = document.querySelector('.docs');
 const textBg = document.querySelector('.textBg');
 
 docsNav.addEventListener("click", function(event) {
+    docs.classList.remove("slide-down");
+    textBg.classList.remove("opacity-down");
+    docs.classList.add("slide-down");
+    textBg.classList.add("opacity-down");
+    slide.style.transform = `translateX(0px)`;
+    nextBtn.style.visibility = 'visible'
+    prevBtn.style.visibility = 'hidden'
+    currentIndex = 0;
+
+    characterdetail.innerHTML = textcharacterdetail
+    characterdescription.innerHTML = textcharacterdescription
+    charactername.innerHTML = textcharactername
+    characterimg.innerHTML = textcharacterimg
+
+
+    setTimeout(function() {
+
     docs.style.display = "none"
     textBg.style.display = "none"
+    }, 400)
+
 })
 textBg.addEventListener("click", function(event) {
+    docs.classList.remove("slide-down");
+    textBg.classList.remove("opacity-down");
+    docs.classList.add("slide-down");
+    textBg.classList.add("opacity-down");
+    slide.style.transform = `translateX(0px)`;
+    nextBtn.style.visibility = 'visible'
+    prevBtn.style.visibility = 'hidden'
+    currentIndex = 0;
+
+    characterdetail.innerHTML = textcharacterdetail
+    characterdescription.innerHTML = textcharacterdescription
+    charactername.innerHTML = textcharactername
+    characterimg.innerHTML = textcharacterimg
+
+    setTimeout(function() {
+
     docs.style.display = "none"
     textBg.style.display = "none"
+    }, 400)
+
 })
+
+const audio = document.getElementById("audio");
+const audioa = document.getElementById("audioa");
+const audiob = document.getElementById("audiob");
+const audioc = document.getElementById("audioc");
+const audiod = document.getElementById("audiod");
+const audios = [audio, audioa, audiob, audioc, audiod]
+
 
 /*fill docs*/
 
@@ -68,57 +118,105 @@ const characterdescription = document.querySelector('.characterdescription');
 const charactername = document.querySelector('.charactername');
 const characterimg = document.querySelector('.characterimg');
 
+let textname = ""
+let textphoto = ""
+let textcaption = ""
+let textdescription = ""
+let textcharacterdetail = ""
+let textcharacterdescription = ""
+let textcharactername = ""
+let textcharacterimg = ""
+
+
 
 listItems.forEach(function(item, index) {
     item.addEventListener("click", function() {
+        docs.classList.remove("slide-up");
+        textBg.classList.remove("opacity-up");
+        docs.classList.remove("slide-down");
+        textBg.classList.remove("opacity-down");
+
+        audios[Math.floor(Math.random() * audios.length)].play();
+        console.log(audio)
+
+        setTimeout(function() {
         docs.style.display = "flex"
         textBg.style.display = "flex"
-        name.innerHTML = data[index].name
-        photo.innerHTML = data[index].photo
-        caption.innerHTML = data[index].caption
-        description.innerHTML = data[index].description
-        characterdetail.innerHTML = data[index].characterdetail
-        characterdescription.innerHTML = data[index].characterdescription
-        charactername.innerHTML = data[index].charactername
-        characterimg.innerHTML = data[index].characterimg
-        
+
+            textname = data[index].name
+            textphoto = data[index].photo
+            textcaption = data[index].caption
+            textdescription = data[index].description
+            textcharacterdetail = data[index].characterdetail
+            textcharacterdescription =  data[index].characterdescription
+            textcharactername =data[index].charactername
+            textcharacterimg = data[index].characterimg
+
+        characterdetail.innerHTML = textcharacterdetail
+        characterdescription.innerHTML = textcharacterdescription
+        charactername.innerHTML = textcharactername
+        characterimg.innerHTML = textcharacterimg
+
+
+        docs.classList.add("slide-up");
+        textBg.classList.add("opacity-up");
+
+
+        }, 200)  
     });
 });
 
 
-const slide = document.querySelector('.slide');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const slideWidth = document.querySelector('.future').clientWidth;
 
+
+const docscolumnleft = document.querySelector('.docs-column-left');
+const docscolumncenter = document.querySelector('.docs-column-center');
+const docscolumnright = document.querySelector('.docs-column-right');
 let currentIndex = 0;
 
-// 이미지 로드 후에 슬라이더의 너비를 가져오기
+//슬라이드 넘기기
 
     
     nextBtn.addEventListener('click', () => {
-        const slideWidth = document.querySelector('.future').clientWidth;
 
-        console.log(slideWidth)
 
         if (currentIndex < 1) {
+
             currentIndex++;
-            slide.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
             nextBtn.style.visibility = 'hidden'
             prevBtn.style.visibility = 'visible'
+
+            docscolumnleft.style.filter = 'invert(82%) sepia(10%) saturate(2466%) hue-rotate(4deg) brightness(92%) contrast(80%)'
+            docscolumncenter.style.filter = 'invert(74%) sepia(4%) saturate(4989%) hue-rotate(184deg) brightness(90%) contrast(84%)'
+            docscolumnright.style.filter = 'invert(40%) sepia(52%) saturate(1280%) hue-rotate(294deg) brightness(96%) contrast(96%)'
+
+            characterdetail.innerHTML = textcaption
+            characterdescription.innerHTML = textdescription 
+            charactername.innerHTML = textname
+            characterimg.innerHTML = textphoto
+
         }
 
     });
     
     prevBtn.addEventListener('click', () => {
 
-        const slideWidth = document.querySelector('.future').clientWidth;
-
         if (currentIndex > 0) {
             currentIndex--;
-            slide.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
             nextBtn.style.visibility = 'visible'
             prevBtn.style.visibility = 'hidden'
+
+            docscolumnleft.style.filter = 'invert(93%) sepia(98%) saturate(5244%) hue-rotate(330deg) brightness(101%) contrast(101%)'
+            docscolumncenter.style.filter = 'invert(76%) sepia(6%) saturate(1243%) hue-rotate(184deg) brightness(111%) contrast(103%)'
+            docscolumnright.style.filter = 'invert(71%) sepia(62%) saturate(215%) hue-rotate(299deg) brightness(103%) contrast(101%)'
+           
+            characterdetail.innerHTML = textcharacterdetail
+            characterdescription.innerHTML = textcharacterdescription
+            charactername.innerHTML = textcharactername
+            characterimg.innerHTML = textcharacterimg
         }
     });
+
+
+
 
